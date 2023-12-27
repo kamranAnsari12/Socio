8f6a5d0d24b8fdb662de4a279310ea8031394c4c
Kamran <kamran@htdigital.in>
2023-12-27 16:16:58 +0530

    header under mui componenets

--- apper/src/assets/Header/Headers.jsx
+++ apper/src/assets/Header/Headers.jsx
@@ -1,24 +1,13 @@
 import React from 'react'
-import '../Header/header.css'
-
+import {Box,Button} from "@mui/material";
 const Headers = () => {
   return (
-    <div>
-      <div className='navbar'>
-        <ul className='horizontalList'>
-          <li className='listItem'>Courses</li>
-          <li className='listItem'>All Exams</li>
-          <li className='listItem'>Current-Affairs</li>
-          <li className='listItem'>Offline Addmission</li>
-          <li className='listItem'>Contact Us</li>
-        </ul>
-        <ul className='horizontalList'>
-          <li className='listItem'>+91 7011808879</li>
-          <li className='listItem'>Go To</li>
-          <li className='listItem'>English</li>
-        </ul>
-      </div>
-    </div>
+   <>
+   <Box sx={{bgcolor:'#414141',color:'white'}}>
+    <Button> this is Button</Button>
+   this is Header
+   </Box>
+   </>
   )
 }
 
